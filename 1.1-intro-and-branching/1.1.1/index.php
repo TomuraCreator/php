7d18baa25b77a1fe3e94f716b436424539e8543d
tery <vladislavutkin92@gmail.com>
2019-10-10 20:06:09 +0300

    внесены изменения, встраивание в HTML

--- 1.1-intro-and-branching/1.1.1/index.php
+++ 1.1-intro-and-branching/1.1.1/index.php
@@ -1,21 +1,24 @@
 <?php 
+    $var = '1';
     $type_array = array('bool', 'float', 'int', 'string', 'null', 'other');
     $descriptions = array('Булево значение. Логический тип данных.',
         'Число с плавающей точкой. Используется в точных математических вычислениях.',
         'Целочисленный тип данных. Используется в простых математических операциях',
         'Строковое представление данных. Представление информации в виде текста.',
         'Специальный тип данных. Означает пееременную без значения.');
-
-    function getsType($var) {
-        global $type_array;
-        global $descriptions;
-        
-        for($i = 0; $i < count($type_array) - 1; $i++) {
-            $types = 'is_'.$type_array[$i];
-            if($types($var)) echo "<p>$var is $type_array[$i]</p><hr>$descriptions[$i]"; 
-        }
-    }
-    getsType(1);
-    getsType(1.1);
-
 ?>
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>bPHP - 1.1.1</title>
+        </head>
+        <body>
+            <?php
+                for($i = 0; $i < count($type_array) - 1; $i++) {
+                    $types = 'is_'.$type_array[$i];
+                    if($types($var)) echo "<p>$var is $type_array[$i]</p><hr>$descriptions[$i]"; 
+                }
+            ?>
+        </body>
+        </html>
