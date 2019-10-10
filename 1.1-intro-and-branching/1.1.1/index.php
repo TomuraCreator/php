4f4d04627f3803b85c33278c3901e204871ea7cf
tery <vladislavutkin92@gmail.com>
2019-10-10 19:51:17 +0300

    решена задача №2

---
+++ 1.1-intro-and-branching/1.1.1/index.php
@@ -0,0 +1,21 @@
+<?php 
+    $type_array = array('bool', 'float', 'int', 'string', 'null', 'other');
+    $descriptions = array('Булево значение. Логический тип данных.',
+        'Число с плавающей точкой. Используется в точных математических вычислениях.',
+        'Целочисленный тип данных. Используется в простых математических операциях',
+        'Строковое представление данных. Представление информации в виде текста.',
+        'Специальный тип данных. Означает пееременную без значения.');
+
+    function getsType($var) {
+        global $type_array;
+        global $descriptions;
+        
+        for($i = 0; $i < count($type_array) - 1; $i++) {
+            $types = 'is_'.$type_array[$i];
+            if($types($var)) echo "<p>$var is $type_array[$i]</p><hr>$descriptions[$i]"; 
+        }
+    }
+    getsType(1);
+    getsType(1.1);
+
+?>
