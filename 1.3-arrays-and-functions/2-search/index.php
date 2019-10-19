9a455284897b1d5aae804b8130ecf5aa1e668624
tery <vladislavutkin92@gmail.com>
2019-10-19 16:42:54 +0300

    Решена задача №2

---
+++ 1.3-arrays-and-functions/2-search/index.php
@@ -0,0 +1,48 @@
+<?php 
+    $map = generate(5, 8);
+    function generate($rows, $placesPerRow) {
+        $arr = [];
+        $counter = 0;
+        for ($i=1; $i <= $rows; $i++) {
+            $arrless = [];
+            for($k=1; $k <= $placesPerRow; $k++) {
+                $randomCount = round(rand(0, 1), 1, PHP_ROUND_HALF_UP);
+                array_push($arrless, ($randomCount == 0) ? FALSE : TRUE);                
+                $counter++;
+            }   
+            $arr[$i] = $arrless;
+        }
+        return $arr;
+    } 
+    
+    function reserveCompany($count, $arr) {
+        
+        for($i = 1; $i <= count($arr); $i++) {
+            $freeSeatCount = 0;
+            if((count($arr[$i]) - ($count + 1)) >= 0):   
+                for($k = 0; $k < count($arr[$i]); $k++) {
+                    if($arr[$i][$k] === false): 
+                       $freeSeatCount++;
+                    else:
+                       $freeSeatCount = 0;
+                    endif;
+                    if($freeSeatCount >= $count):
+                        echo "Свободные места есть в $i ряду";
+                        exit;
+                    elseif($i === count($arr)): 
+                        echo "Свободных мест нет";
+                        exit;
+                    endif;
+                }
+            endif;
+            
+            
+        }
+    }
+    var_dump($map);
+    reserveCompany(4, $map);
+
+    
+
+
+?>
