7091b4af13e80681ec2caa66abc5c9fa294c210c
tery <vladislavutkin92@gmail.com>
2019-11-01 16:34:19 +0300

    исправлено

--- 2.3-dates-and-sessions/2.3.1-your-tube/index.php
+++ 2.3-dates-and-sessions/2.3.1-your-tube/index.php
@@ -1,5 +1,5 @@
 <?php
-    error_reporting(E_ALL & ~E_NOTICE, ~E_WARNING);
+    error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
     /**
      * Функция получает текущее количество просмотров на видео
      *
@@ -33,7 +33,7 @@
         $expire = 300;
         
         $timer = $_COOKIE['timer'];
-        if( !empty($timer)) {
+        if( !empty($timer) )  {
             setcookie('timer', time(), time()+$expire);
         }
 
